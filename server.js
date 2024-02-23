bc4e1820a1a7001cd047e578921729f0ccc6ee74
Guenier Mathieu <guenier.mathieu2@gmail.com>
2024-02-23 11:27:08 +0100

    server update

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
+app.use(express.static(__dirname+'/www'));
+
 app.listen(port, () => {
     console.log('Le server est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
-app.get('/', (req, res, next) =>{
-    res.send('Bonjour tout le monde ! Je vous nique bande de cuck !')
-})
+
+app.use('/js', express.static(__dirname+'/node_modules/bootstrap/dist/js')); 
+app.use('/js', express.static(__dirname+'/node_modules/bootstrap/dist/css')); 
+app.use('/js', express.static(__dirname+'/node_modules/jquery/dist')); 
+app.use('/js', express.static(__dirname+'/node_modules/@popperjs/core/dist/umd')); 
+app.use('/css', express.static(__dirname+'/node_modules/bootstrap/dist/css'));
+
+app.get('/', (req, res , next) =>{ 
+    res.sendFile('/www/index.html'); 
+});
